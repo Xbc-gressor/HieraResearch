9a5e572e7c7ed3c9b2d682b700517a74c42f8584
DanJoshua <wodenwei@outlook.com>
2026-08-24 09:26:15 +0800

    refactor(benchmark): move standalone YAHPO pilot out of the repo

--- tools/yahpo_benchmark/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Standalone YAHPO Gym benchmark for HEBO and LLM-assisted optimizers."""
-
-from .suite import PILOT_TASKS, SMOKE_TASKS, TaskSpec
-
-__all__ = ["PILOT_TASKS", "SMOKE_TASKS", "TaskSpec"]
